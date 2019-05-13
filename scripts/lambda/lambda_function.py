d157aa8255f798aede0f8aae7e8f4942b99c3b78
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2019-05-13 14:11:42 -0700

    Delete lambda_function.py
    not required. old version

--- scripts/lambda/lambda_function.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-import json
-import boto3
-
-client = boto3.client('glue')
-
-def lambda_handler(event, context):
-    # TODO implement
-    response = client.start_job_run(JobName = 'fire_etl2')
